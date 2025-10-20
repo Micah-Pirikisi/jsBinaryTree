ae09def8a7547a627f4c4515cd5221b797bb513e
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-20 08:39:27 +0200

    Added removeDuplicates(array) to class Tree

--- index.js
+++ index.js
@@ -13,4 +13,14 @@ class Tree {
 
     this.root = this.buildTree(uniqueArray);
   }
+
+  removeDuplicates(array) {
+    const result = []; 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== array[i - 1]) {
+            result.push(array[i]); 
+        }
+    }
+    return result; 
+  }
 }
