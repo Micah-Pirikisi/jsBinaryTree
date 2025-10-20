cef2ad4e3d86257b70f7b6586de4f08c696dd9ad
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-20 08:44:12 +0200

    Added buildTree(array) to class Tree

--- index.js
+++ index.js
@@ -15,12 +15,24 @@ class Tree {
   }
 
   removeDuplicates(array) {
-    const result = []; 
+    const result = [];
     for (let i = 0; i < array.length; i++) {
-        if (array[i] !== array[i - 1]) {
-            result.push(array[i]); 
-        }
+      if (array[i] !== array[i - 1]) {
+        result.push(array[i]);
+      }
     }
-    return result; 
+    return result;
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const midIndex = Math.floor(array.length / 2);
+    const root = new Node(array[mid[Index]]);
+
+    root.left = this.buildTree(array.slice(0, midIndex));
+    root.right = this.buildTree(array.slice(midIndex + 1));
+
+    return root;
   }
 }
